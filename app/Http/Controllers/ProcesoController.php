4612d9d11a2f340ae8b547c08b9e27b95f605810
Leonard Norton <mrgriffin001001@gmail.com>
2021-04-11 17:05:13 -0500

    add body to controllers

--- app/Http/Controllers/ProcesoController.php
+++ app/Http/Controllers/ProcesoController.php
@@ -2,9 +2,102 @@
 
 namespace App\Http\Controllers;
 
+use App\Proceso;
 use Illuminate\Http\Request;
 
 class ProcesoController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'empresa_id'=>'required|exists:empresas',
+            'tipo_proceso_id'=>'required|exists:tipo_procesos',
+            'nombre'=>'required',
+
+        ],
+            [
+                'required'=>'Debe especificar un :attribute',
+                'exists'=>':attribute debe existir en la base de datos',
+                'filled'=>':attribure debe contener elementos',
+            ]
+        );
+        $proceso = new Proceso();
+        $proceso->empresa_id =$request->empresa_id;
+        $proceso->tipo_proceso_id =$request->tipo_proceso_id;
+        $proceso->nombre =$request->nombre;
+        $proceso->save();
+
+        return $proceso;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
